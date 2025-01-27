db34417764dd965b49be0c82468cd8dcc58c7f21
omarsanchez <omarcore@gmail.com>
2025-01-27 16:38:20 -0600

    Mostrando listado de posts

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -23,6 +23,7 @@ class TagFactory extends Factory
         return [
             'name' => $name,
             'slug' => Str::slug($name),
+            'color' => $this->faker->randomElement(['red', 'yellow', 'green', 'black', 'blue','indigo','purple','pink'])
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 Route::middleware([
     'auth:sanctum',
