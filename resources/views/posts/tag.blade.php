2ee88b53b35e016aa33fddaa59b7888e07fa216c
omarsanchez <omarcore@gmail.com>
2025-01-29 10:07:52 -0600

    Agregando listado de post filtrados por tags

--- resources/views/posts/tag.blade.php
+++ resources/views/posts/tag.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
 
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-8">
-        <h1 class="uppercase text-center text-2xl font-bold pb-4">Categoría:
+    <div class="mx-auto max-w-5xl px-2 sm:px-6 lg:px-8 py-8">
+        <h1 class="uppercase text-center text-2xl font-bold pb-4">Etiqueta:
             <span class="text-indigo-800 text-4xl">
-                {{$category->name}}
+                {{$tag->name}}
             </span> 
         </h1>
 
-        <div class="grid md:grid-cols-3">
+        <div class="grid md:grid-cols-2">
 
             @foreach ($posts as $post)
     
@@ -15,6 +15,7 @@
                 
             @endforeach
         </div>
+
 
         <div class="mt-4">
             {{$posts->links()}}
