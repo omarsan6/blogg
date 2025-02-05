7581e22b966c31a639559553789df703d6f8fec0
omarsanchez <omarcore@gmail.com>
2025-02-05 12:33:12 -0600

    Mostrando listado de posts con filtrado

--- app/Livewire/Admin/PostsIndex.php
+++ app/Livewire/Admin/PostsIndex.php
@@ -6,14 +6,30 @@ use App\Models\Post;
 
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
-
+use Livewire\WithPagination;
 
 class PostsIndex extends Component
 {
+
+    // paginacion
+    use WithPagination;
+
+    // cambiar los estilos de paginacion en vez de tailwind a bootstrap
+    protected $paginationTheme = "bootstrap";
+
+    public $search;
+
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+
     public function render()
     {
 
-        $posts = Post::where('user_id', Auth::id())->paginate();
+        $posts = Post::where('user_id', Auth::id())
+                    ->where('name', 'LIKE', '%' . $this->search . '%')
+                    ->latest('id')
+                    ->paginate();
 
         return view('livewire.admin.posts-index',compact('posts'));
     }
--- resources/views/livewire/admin/posts-index.blade.php
+++ resources/views/livewire/admin/posts-index.blade.php
@@ -1,36 +1,56 @@
 <div class="card">
-    <div class="card-body">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th colspan="2">Acciones</th>
-                </tr>
-            </thead>
 
-            <tbody>
-
-                @foreach ($posts as $post)
-                    <tr>
-                        <td>{{$post->id}}</td>
-                        <td>{{$post->name}}</td>
-                        <td width="10px">
-                            <a class="btn btn-primary btn-sm" href="{{route('admin.posts.edit',$post)}}">Editar</a>
-                        </td>
-                        <td width="10px">
-                            <form action="{{route('admin.posts.destroy',$post)}}" method="POST">
-                                @csrf
-                                @method('DELETE')
-
-                                <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
-
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-
-            </tbody>
-        </table>
+    <div class="card-header">
+        <input wire:model.live='search' type="text" class="form-control" placeholder="Buscar post">
     </div>
+
+    @if ($posts->count())
+        
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th colspan="2">Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    @foreach ($posts as $post)
+                        <tr>
+                            <td>{{$post->id}}</td>
+                            <td>{{$post->name}}</td>
+                            <td width="10px">
+                                <a class="btn btn-primary btn-sm" href="{{route('admin.posts.edit',$post)}}">Editar</a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{route('admin.posts.destroy',$post)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button class="btn btn-danger btn-sm" type="submit">Eliminar</button>
+
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </tbody>
+            </table>
+        </div>
+
+        <div class="card-footer">
+            {{$posts->links()}}
+        </div>
+    @else
+
+        <div class="card-body">
+            <p class="text-secondary font-weight-bold">No hay ningún registro</p>
+        </div>
+
+
+    @endif
+
 </div>
