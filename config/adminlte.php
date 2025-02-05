22a2d673ae35ea3c3a46cb479477c42bfa0cff72
omarsanchez <omarcore@gmail.com>
2025-02-05 11:27:56 -0600

    Configuración de Controlador, rutas y vistas de posts

--- config/adminlte.php
+++ config/adminlte.php
@@ -336,59 +336,16 @@ return [
             'route' => 'admin.tags.index',
             'icon' => 'far fa-fw fa-bookmark',
         ],
+        ['header' => 'OPCIONES DE BLOG'],
         [
-            'text' => 'multilevel',
-            'icon' => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                        ],
-                        [
-                            'text' => 'level_two',
-                            'url' => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url' => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url' => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text' => 'important',
-            'icon_color' => 'red',
-            'url' => '#',
+            'text' => 'Lista de Post',
+            'route' => 'admin.posts.index',
+            'icon' => 'fas fa-fw fa-clipboard',
         ],
         [
-            'text' => 'warning',
-            'icon_color' => 'yellow',
-            'url' => '#',
-        ],
-        [
-            'text' => 'information',
-            'icon_color' => 'cyan',
-            'url' => '#',
+            'text' => 'Nuevo Post',
+            'route' => 'admin.posts.create',
+            'icon' => 'fas fa-fw fa-file',
         ],
     ],
 
