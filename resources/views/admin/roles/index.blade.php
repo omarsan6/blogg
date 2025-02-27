a538ba5daa4329f6e9c86fcbaa1f0d3fb11b6c43
omarsanchez <omarcore@gmail.com>
2025-02-27 09:49:23 -0600

    Vista de roles

---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'LunaDev')
+
+@section('content_header')
+    <h1>Roles</h1>
+@stop
+
+@section('content')
+    <p></p>
+@stop
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
