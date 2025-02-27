579775951c69ede4eb966aff8454fcee776889cb
omarsanchez <omarcore@gmail.com>
2025-02-27 10:04:49 -0600

    Listado de roles

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -13,7 +13,8 @@ class RoleController extends Controller
      */
     public function index()
     {
-        return view('admin.roles.index');
+        $roles = Role::all();
+        return view('admin.roles.index',compact('roles'));
     }
 
     /**
--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -3,11 +3,55 @@
 @section('title', 'LunaDev')
 
 @section('content_header')
-    <h1>Roles</h1>
+
+    <div class="d-flex w-100">
+        <h1 class="mr-3">Roles</h1>
+        <a href="{{route('admin.roles.create')}}" class="btn btn-success">Nuevo</a>
+    </div>
 @stop
 
 @section('content')
-    <p></p>
+    
+    <div class="card">
+        <div class="card-body">
+            <table class="table tabel-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Role</th>
+                        <th colspan="2">Acciones</th>
+                    </tr>
+                </thead>
+
+                <t-body>
+
+                    @foreach ($roles as $role)
+                        <tr>
+                            <td>
+                                {{$role->id}}
+                            </td>
+                            <td>
+                                {{$role->name}}
+                            </td>
+                            <td width="10px">
+                                <a class="btn btn-sm btn-primary" href="{{route('admin.roles.edit',$role)}}">Editar</a>
+                            </td>
+                            <td width="10px">
+                                <form action="{{route('admin.roles.destroy',$role)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button class="btn btn-sm btn-danger" type="submit">Eliminar</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                </t-body>
+            </table>
+        </div>
+    </div>
+
 @stop
 
 @section('css')
