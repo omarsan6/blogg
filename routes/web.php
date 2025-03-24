d7c36b3038158217db43f922aed72d571cbaee86
omarsanchez <omarcore@gmail.com>
2025-03-24 10:00:17 -0600

    Mostrando pantalla principal

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,14 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/', function(){
+
+    return view('welcome');
+
+});
+
+Route::get('/blog', [PostController::class, 'index'])->name('posts.index');
+
 Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
 Route::get('category/{category}', [PostController::class, 'category'])->name('posts.category');
