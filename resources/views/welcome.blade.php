597c99a42b7901667302ba4f01b09248a424b96a
omarsanchez <omarcore@gmail.com>
2025-03-26 09:32:45 -0600

    Arreglando componente servicios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -314,7 +314,7 @@
                 <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
                     <a href="#" class="flex flex-wrap no-underline hover:no-underline">
                         <div class="w-full font-bold text-xl text-gray-800 px-6">
-                            Desarrollo web (landing pages, blogs)
+                            Desarrollo web (landing pages, blogs, solución personalizada)
                         </div>
                         <p class="text-gray-800 text-base px-6 mb-5">
                             Creamos sitios web para impulsar la presencia online de tu negocio 
@@ -323,7 +323,7 @@
                     </a>
                 </div>
                 <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-                    <div class="flex items-center justify-start">
+                    <div class="flex items-center justify-center">
                         <button
                             class="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
                             Contáctanos
